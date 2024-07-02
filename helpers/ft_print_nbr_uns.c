650d348e13a77469c0c5e451940143aef53481b1
乃ㄥ4乙3_ㄚ <99927294+yes-slim@users.noreply.github.com>
2024-07-02 21:12:07 +0000

    check

--- helpers/ft_print_nbr_uns.c
+++ helpers/ft_print_nbr_uns.c
@@ -19,6 +19,6 @@ void	ft_print_nbr_uns(unsigned int i, int *len)
 		ft_print_nbr_uns((i / 10), len);
 		ft_print_nbr_uns((i % 10), len);
 	}
-	if (i >= 0 && i <= 9)
+	if (i > 0 && i <= 9)
 		ft_print_char((i + '0'), len);
 }
